7c1739be6e97ef19cdc0461138d8b9d7f09289ce
sanjana-bhat-2002 <asanjanabbhat@gmail.com>
2024-08-06 21:14:31 +0530

    updated dailyupdates endpoint

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 // Schedule a job to run every 30 seconds
 cron.schedule('30 * * * * *', async () => {
   try {
-    const response = await fetch('http://localhost:3000/api/dailyUpdates', {
+    const response = await fetch('https://wordrix.vercel.app/api/dailyUpdates', {
       method: 'GET',
     });
 
